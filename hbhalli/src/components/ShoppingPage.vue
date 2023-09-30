10941e95d855b031dea61856cc3422d254ccdaff
bhauhbbh <66502591+bhauhbbh@users.noreply.github.com>
2023-09-30 17:31:39 +0530

    changes

--- hbhalli/src/components/ShoppingPage.vue
+++ hbhalli/src/components/ShoppingPage.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container class="bg-grey-lighten-4">
+    <h2 class="mt-12 mb-4 pa-4 blue-grey-darken-1 text-center">Vegitables 1</h2>
     <v-row justify="center">
       <v-col v-for="(_, n) in 12" :key="n" cols="12" md="4" lg="3">
         <v-card rounded="lg" class="mx-auto elevation-0" max-width="300">
@@ -25,7 +26,59 @@
                 <v-select
                   density="compact"
                   label="Quantity"
-                  :items="['1 Kg', '2 Kg', '3 Kg', '4 Kg', '5 Kg', '6 Kg']"
+                  :items="[
+                    '1/2 Kg',
+                    '1 Kg',
+                    '2 Kg',
+                    '3 Kg',
+                    '4 Kg',
+                    '5 Kg',
+                    '6 Kg',
+                  ]"
+                ></v-select>
+              </v-sheet>
+            </v-col> </v-row
+          ><v-card-actions>
+            <v-btn
+              block
+              rounded="lg"
+              color="green-darken-1"
+              append-icon="mdi mdi-cart-plus"
+              variant="flat"
+              class="text-none text-h6"
+            >
+              Add to cart
+              <template v-slot:append>
+                <v-icon color="white"></v-icon>
+              </template>
+            </v-btn>
+          </v-card-actions> </v-card
+      ></v-col>
+    </v-row>
+    <h2 class="mt-12 mb-4 pa-4 blue-grey-darken-1 text-center">Vegitables 2</h2>
+    <v-row justify="center">
+      <v-col v-for="(_, n) in 8" :key="n" cols="12" md="4" lg="3">
+        <v-card rounded="lg" class="mx-auto elevation-0" max-width="300">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="150px"
+            cover
+          >
+          </v-img>
+          <v-card-title class="text-h6 font-weight-black"> Soppu </v-card-title>
+          <v-row no-gutters>
+            <v-col>
+              <v-sheet class="mx-auto">
+                <v-card-text class="text-h6">
+                  Rs 65 <span class="text-disabled"></span> </v-card-text
+              ></v-sheet>
+            </v-col>
+            <v-col>
+              <v-sheet class="mx-auto pe-3">
+                <v-select
+                  density="compact"
+                  label="Quantity"
+                  :items="['1', '2', '3', '4', '5', '6']"
                 ></v-select>
               </v-sheet>
             </v-col> </v-row
@@ -50,51 +103,7 @@
 </template>
 
 <script>
-// import firebase from 'firebase/app';
-// import 'firebase/database';
-export default {
-  // data() {
-  //   return {
-  //     items: [],
-  //   };
-  // },
-  // created() {
-  //   // Reference to the Firebase database
-  //   const db = firebase.database();
-  //   // Reference to a specific database node
-  //   const itemsRef = db.ref('items');
-  // Fetch data once
-  // itemsRef
-  //   .once('value')
-  //   .then((snapshot) => {
-  //     const data = snapshot.val();
-  //     if (data) {
-  //       // Convert Firebase data to an array
-  //       this.items = Object.entries(data).map(([key, value]) => ({
-  //         key,
-  //         value,
-  //       }));
-  //     } else {
-  //       this.items = [];
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     console.error('Error fetching data:', error);
-  //   });
-  // },
-  //   props: {
-  //     product: {
-  //       type: Object,
-  //       required: true,
-  //     },
-  //   },
-  //   data() {
-  //     return {
-  //       selectedQuantity: 0.5, // Default quantity selected
-  //       quantityOptions: [0.5, 1, 1.5, 2, 2.5, 3], // Customize the available quantity options as needed
-  //     };
-  //   },
-};
+export default {};
 </script>
 
 <style></style>
